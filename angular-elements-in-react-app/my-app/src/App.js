f4a37d6c444348ac941926dd14219bec5139c022
Justin Paoletta <jpaoletta.dev@gmail.com>
2021-10-13 16:12:40 -0400

    Add files via upload

---
+++ angular-elements-in-react-app/my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/element-one/dist/element-one/element-one';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <element-one/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
